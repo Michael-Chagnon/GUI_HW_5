cb608deecec046406e905dcdfd852353e814a5c6
Michael-Chagnon <michaelchagnon12@gmail.com>
2023-12-05 00:04:05 -0500

    dec5 12:03 backup

--- js/script.js
+++ js/script.js
@@ -10,24 +10,74 @@ Description: Scrabble
 */
 
 function makeDrag(){
-    $( ".tile" ).draggable({snap: ".snapPoint" , snapMode:"inner"});
-    $( ".snapPoint" ).droppable({snap: true});
+    $( ".tile" ).draggable({
+    snap: ".snapPoint",
+    snapMode: "inner",
+    drag: function(event, ui) {
+        $(this).removeClass("onRack");
+        $(this).addClass("offRack");
+    }
+    });
+
+    $( ".snapPoint" ).droppable({snap: true,
+        drop: function(event, ui) {
+            $(ui.draggable).removeClass("offRack");
+            $(ui.draggable).addClass("onRack");
+        }
+    });
+
+    $( ".boardPoint" ).droppable({snap: true,
+        drop: function(event, ui) {
+            $(ui.draggable).removeClass("offRack");
+            $(ui.draggable).removeClass("onRack");
+            $(ui.draggable).addClass("onBoard");
+            $("#" + ui.draggable.attr("id")).detach().appendTo("#boardSpot1");
+            // alert();
+        },
+    });
+
+    $( ".boardSnapPoint" ).droppable({snap: true});
 }
 
-$( function() {
-    makeDrag();
-});
-
-// $( function() {
-//     $( ".holderPiece1" ).droppable({snap: true});
-// });
+$("#holderPiece1").droppable({
+    drop: function(event, ui) {
+        $("#holderPiece1").addClass("Occupied");
+    }
+  });
 
 
 $("button#newPieces").click(function() {
     var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Blank"]
-    var number = parseInt(Math.random() * (27 - 0) + 0);
-    $("div#pieceImage").append("<img id='tile' class='" + "tile" + "'" + "src='Scrabble_Tiles/Scrabble_Tile_" + letters[number] + ".jpg'" + "'>" + "</img>");
-    var destination = $('.snapPoint').offset().top;
-    $('.tile').css({top: destination.top, left: destination.left}); 
+    var number;
+    $('.onRack').remove();
+    var numTileNeeded = Math.abs($('#pieceHolder .onRack').length - 7);
+    
+
+    if($('#pieceHolder .onRack').length == 7){
+        // var location = document.getElementsByClassName("onRack");
+        for(var i = 0; i < numTileNeeded; i++){
+            number = parseInt(Math.random() * (27 - 0) + 0);
+            $("div#pieceImage").append("<img id='tile" + letters[number] +"' class='" + "tile onRack" + "'" + "src='Scrabble_Tiles/Scrabble_Tile_" + letters[number] + ".jpg'" + "'>" + "</img>");
+            var destination = $('.snapPoint').offset().top;
+            $('.tile').css({top: destination.top, left: destination.left}); 
+            makeDrag();
+        }
+    } else {
+        for(var i = 0; i < numTileNeeded; i++){
+            number = parseInt(Math.random() * (27 - 0) + 0);
+            $("div#pieceImage").append("<img id='tile" + letters[number] +"' class='" + "tile onRack" + "'" + "src='Scrabble_Tiles/Scrabble_Tile_" + letters[number] + ".jpg'" + "'>" + "</img>");
+            var destination = $('.snapPoint').offset().top;
+            $('.tile').css({top: destination.top, left: destination.left}); 
+            makeDrag();
+        }
+    }
+});
+
+$("button#newGame").click(function() {
+    location.reload();
+});
+
+
+$( function() {
     makeDrag();
 });
